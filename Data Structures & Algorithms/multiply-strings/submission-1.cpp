e88ca03fb2085d1d015505eb9cd822e68258a389
komal-kumawat <154998527+komal-kumawat@users.noreply.github.com>
2026-05-20 01:33:54 +0530

    Add: multiply-strings - submission-1

---
+++ Data Structures & Algorithms/multiply-strings/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        int m = num1.size() , n = num2.size();
+        vector<int>res(m+n ,0);
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                int mul = (num1[i]-'0')*(num2[j]-'0');
+                int sum = mul+res[i+j+1];
+                res[i+j+1] = sum%10;
+                res[i+j] +=sum/10;
+            }
+        }
+        string ans = "";
+        
+        for(int i=0;i<(m+n);i++){
+            if(!(ans.empty() &&res[i]==0)){
+                ans+=to_string(res[i]);
+                
+            }
+
+
+        }
+        return (ans.empty())?"0":ans;
+        
+    }
+};
