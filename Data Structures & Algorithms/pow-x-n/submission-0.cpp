dccf7926ee732b9a5cc60b3d87b3c99d0d3c4f21
komal-kumawat <154998527+komal-kumawat@users.noreply.github.com>
2026-05-20 01:18:00 +0530

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    double solve(double x, long long n) {
+
+        if(n == 0) return 1;
+
+        double half = solve(x, n/2);
+
+        if(n % 2 == 0) {
+            return half * half;
+        }
+
+        return x * half * half;
+    }
+
+    double myPow(double x, int n) {
+
+        long long N = n;
+
+        // handle negative power
+        if(N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        return solve(x, N);
+    }
+};
