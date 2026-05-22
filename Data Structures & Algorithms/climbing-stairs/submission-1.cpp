97e6f92460e1244c1a08d6321358230622ccd25b
komal-kumawat <154998527+komal-kumawat@users.noreply.github.com>
2026-05-22 12:14:04 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==1) return 1;
+        if(n==2) return 2;
+        int first = 1 , second = 2;
+        for(int i=3;i<=n;i++){
+            int temp = first;
+            first = second;
+            second = temp+first;
+        }
+        return second;
+        
+    }
+};
