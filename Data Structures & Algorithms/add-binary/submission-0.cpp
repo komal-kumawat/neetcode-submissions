05ba53e298c945839db756e90ee81b75ee2f88d4
komal-kumawat <154998527+komal-kumawat@users.noreply.github.com>
2026-05-30 22:16:27 +0530

    Add: add-binary - submission-0

---
+++ Data Structures & Algorithms/add-binary/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string ans = "";
+        int i = a.size()-1 , j = b.size()-1 , carry = 0;
+        while(i>=0 || j>=0 || carry){
+            int sum = carry;
+            if(i>=0){
+                sum+=(a[i]-'0');
+                i--;
+            }
+            if(j>=0){
+                sum+=(b[j]-'0');
+                j--;
+            }
+            carry = sum/2;
+            sum = sum%2;
+            if(sum==1){
+                ans = '1'+ans;
+            }else{
+                ans = '0'+ans;
+            }
+        }
+        return ans;
+        
+    }
+};
