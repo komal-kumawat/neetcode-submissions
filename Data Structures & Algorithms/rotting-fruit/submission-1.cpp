7668b2c16fb443a09d62eb1c8101af5001aa5618
komal-kumawat <154998527+komal-kumawat@users.noreply.github.com>
2026-05-31 23:53:57 +0530

    Add: rotting-fruit - submission-1

---
+++ Data Structures & Algorithms/rotting-fruit/submission-1.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<int>>distArr = {{-1 , 0} , {1,0} , {0 , 1} , {0,-1}};
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size() , n = grid[0].size();
+        queue<pair<int,int>>q;
+        int fresh = 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==2){
+                    q.push({i , j});
+                }else if(grid[i][j]==1){
+                    fresh++;
+                }
+            }
+        }
+        int time = 0;
+        while(!q.empty()){
+            int sz = q.size();
+            for(int i=0;i<sz;i++){
+                auto [x , y] = q.front();
+                q.pop();
+                for(int j=0;j<4;j++){
+                    int dx = x+distArr[j][0];
+                    int dy = y+distArr[j][1];
+                    if(dx<0 || dy<0 || dx>=m || dy>=n || grid[dx][dy]==0||grid[dx][dy]==2){
+                        continue;
+                    }
+                    grid[dx][dy] = 2;
+                    fresh--;
+                    q.push({dx , dy});
+                }
+            }
+            time++;
+
+
+        }
+        
+        
+        return fresh==0?max(time-1 , 0):-1;
+        
+    }
+};
