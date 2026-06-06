61ce06f817e92047d97b9c934b9a8da8cc03c4c0
komal-kumawat <154998527+komal-kumawat@users.noreply.github.com>
2026-06-07 01:19:45 +0530

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+        while(n>0){
+            if((n&1)==1){
+                count++;
+            }
+            n = n>>1;
+        }
+        return count;
+        
+    }
+    public int[] countBits(int n) {
+        int[] ans = new int[n+1];
+        for(int i=1;i<=n;i++){
+            ans[i] = hammingWeight(i);
+        }
+        return ans;
+        
+    }
+}
